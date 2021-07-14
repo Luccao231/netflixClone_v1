f9b325cced81ca302004f1b9b5705d4a672e6e1d
Luccao <luccamelo231@gmail.com>
2021-07-14 16:03:12 -0300

    Inicializando o projeto, terá atualizações

---
+++ script.js
@@ -0,0 +1,32 @@
+new Glider(document.querySelector('.glider_1'), {
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  });
+  
+  new Glider(document.querySelector('.glider_2'), {
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  });
+  
+  new Glider(document.querySelector('.glider_3'), {
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  });
